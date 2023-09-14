d72c34858aa5c41b6314ce3ace9f18513a924da8
seon318 <140936963+seon318@users.noreply.github.com>
2023-09-14 11:42:21 +0900

    day1

---
+++ Day1/1-3 Inputs.py
@@ -0,0 +1,2 @@
+input("What is your name? ")
+print(len(input("What is your name? "))) #문자열 길이 계산
---
+++ Day1/1-p Band name Generator.py
@@ -0,0 +1,14 @@
+#1. Create a greeting for your program.
+print("Hello " + input("What is your name?"))
+
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is " + city + pet)
+
+#5. Make sure the input cursor shows on a new line:
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
